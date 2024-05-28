0747144b32a3243c9fd7a6b58ec480051cc08530
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-05-28 17:41:51 +0200

    add an application section to the project which includes detailed information about me to apply for the course, add application sections's styles and add event listener related to this section to make it interactice

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("click", function (e) {
+    if (e.target.closest(".section-header")) {
+        const currentCollapsible = e.target.closest(".collapsible");
+
+        // If the element which was clicked is already active, remove the active class and return
+        if (currentCollapsible.classList.contains("active")) {
+            currentCollapsible.classList.remove("active");
+            return;
+        }
+
+        // Remove the active class in the collapsible elements
+        document.querySelectorAll(".collapsible").forEach((collapsible) => {
+            collapsible.classList.remove("active");
+        });
+
+        // When clicking on a collapsible element, add or remove active class
+        currentCollapsible.classList.add("active");
+    }
+});
