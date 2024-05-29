fe6746b4c1e8cab964a817dd96002279856d385b
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-05-29 21:30:51 +0200

    add flip card structure to index.html and script.js

--- script.js
+++ script.js
@@ -26,4 +26,14 @@ document.addEventListener("click", function (e) {
 });
 
 
+/* Flip card */
+const front = document.getElementById('front')
+const back = document.getElementById('back')
+const btn = document.getElementById('flip-btn')
 
+function handleFlip() {
+  front.classList.toggle('flipped')
+  back.classList.toggle('flipped')
+}
+
+btn.addEventListener('click', handleFlip)
